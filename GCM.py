6144213577477d43600ec7bcfe8fcda77dba41d6
LightEdgeRK <roozbehketabi@gmail.com>
2014-11-08 13:39:12 -0500

    Corrected JSON request (got 200 status reply)

--- GCM.py
+++ GCM.py
@@ -1,9 +1,20 @@
 from httplib2 import Http
-from urllib import urlencode
+
+try:
+    # For c speedups
+    from simplejson import loads, dumps
+except ImportError:
+    from json import loads, dumps
 
 def GCMSend(UserGCMID,Message):
     h = Http()
-    headers = {'Content-type' : 'application/json' , 'Authorization' : 'key=' + APIKEY} #APIKEY From GCM API
-    data = dict(registration_ids = UserGCMID, data=Message) #if didn't work, try registration id to be a list it self
-    resp, content = h.request("https://android.googleapis.com/gcm/send", "POST", headers, urlencode(data))
+    headers = { 'Content-type' : 'application/json' ,
+                'Authorization' : 'key=' + 'AIzaSyAa5sLIry4DAILEmtESUjw88eh40urZkog'
+              }; #APIKEY From GCM API
+    data = dict(registration_ids = [ UserGCMID ], data = {'message': Message} ) #if didn't work, try registration id to be a list it self
+    print data
+    resp, content = h.request("https://android.googleapis.com/gcm/send", "POST", headers = headers, body = dumps(data) )
+    print resp
+    print content
 
+GCMSend('APA91bE4Kglfn6ntxLdewmSpsAI1rwnrYpffjWR_KpYGfnWhYFgxphZz3_j4-KvW2y6rYTAeqZfgvBmL-rnPIyAXbdbMxsc2y597bDVAM1YF8COBHvMVoJM6P-x2OQLFmlWvIWx24svxDlm6WeIn7ZqsTiSDKi7WhA','HELLO!')
